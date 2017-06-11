f00148fc1ade6f01d29d38a3c4c7d7752a83da68
Kaya <ubersensei@gmail.com>
2017-06-11 10:36:05 +0800

    Initial App Structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './components/routes';
+import './styles/styles.css'; //Webpack can import CSS files too!
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+render(
+    <Router history={browserHistory} routes={routes}/>,
+    document.getElementById('app')
+);
