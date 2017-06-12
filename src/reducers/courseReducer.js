a7a01b110d723b21fad6c911922e506fd060d39f
Kaya <ubersensei@gmail.com>
2017-06-12 11:36:38 +0800

    Redux Flow

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,15 @@
+import * as types from '../actions/actionTypes';
+
+
+export default function courseReducer(state = [], action) {
+    switch (action.type) {
+        case types.CREATE_COURSE:
+            return [...state, Object.assign({}, action.course)];
+
+        // state.push(action.course);
+        // return state;
+
+        default:
+            return state;
+    }
+}
